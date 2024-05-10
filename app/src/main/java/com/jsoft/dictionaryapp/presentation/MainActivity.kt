046e0dd99c4bce1f7c54ae40332e9ede8791c151
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-05-10 14:00:44 +0500

    ...

--- app/src/main/java/com/jsoft/dictionaryapp/presentation/MainActivity.kt
+++ app/src/main/java/com/jsoft/dictionaryapp/presentation/MainActivity.kt
@@ -183,5 +183,6 @@ class MainActivity : ComponentActivity() {
 
         }
     }
-
 }
+
+
--- app/src/main/java/com/jsoft/dictionaryapp/presentation/event/MainUiEvent.kt
+++ app/src/main/java/com/jsoft/dictionaryapp/presentation/event/MainUiEvent.kt
@@ -3,5 +3,5 @@ package com.jsoft.dictionaryapp.presentation.event
 sealed class MainUiEvents {
 
     data class OnSearchWordChange(val newWord: String) : MainUiEvents()
-    object OnSearchClick : MainUiEvents()
+    data object OnSearchClick : MainUiEvents()
 }
